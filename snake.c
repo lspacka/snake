a3f08a2b6746be3c1c2dfd8bb9090e2017ed5a71
julian <jdrc@protonmail.com>
2025-06-18 19:17:00 -0500

    add growing logic

--- snake.c
+++ snake.c
@@ -1,6 +1,9 @@
 //  -lraylib -lgdi32 -lwinmm -Wall -std=c99 -I c:/raylib/raylib/src
 
+//  - change speed to vector
+
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include "raylib.h"
@@ -10,7 +13,8 @@ typedef  struct Snake {
     Vector2 pos;
     float xspeed;
     float yspeed;
-    // Snake* tail;
+    int capacity;
+    Vector2* tail ;
 } Snake;
 
 void SnakeDir(Snake* snake, float speed);
@@ -24,14 +28,16 @@ int main()
 
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(width, height, "Snake");
-    SetTargetFPS(8);
+    SetTargetFPS(13);
 
+    int i;
     int scale = 20;
-    int total = 1;
-    float speed = 1.0f;
+    int total = 0;
+    // int capacity = 100;
+    int speed = 1;
     Snake snake = {0};
-    // Snake* snake = malloc(total * sizeof(Vector2));
-    Snake* tail = malloc(total * sizeof(Vector2));
+    snake.capacity = (width/scale) * (height/scale);
+    snake.tail = malloc(snake.capacity * sizeof(Vector2));
     Vector2 food = {0};
     // Color pinky = {233, 30, 99};
 
@@ -41,6 +47,49 @@ int main()
     PickLocation(&food, width, height, scale);
 
     while (!WindowShouldClose()) {
+        if (Vector2Distance(snake.pos, food) < 1) {
+            PickLocation(&food, width, height, scale);
+            total++;
+            // size_t new_size = total * sizeof(Vector2);
+            // snake.tail = realloc(snake.tail, new_size);
+            // if (total > snake.capacity) {
+            //     snake.capacity *= 2;
+            //     size_t new_size = snake.capacity * sizeof(Vector2);
+            //     snake.tail = realloc(snake.tail, new_size);
+            // }
+
+            //  check tail array resizing
+            // if (snake.tail == NULL) 
+            //     printf("tail>0 NULL\n\n");
+            // else
+            //     printf("%zu\n", new_size);
+
+            // init new tail segment to current head position
+            if (total > 0) 
+                snake.tail[total-1] = snake.pos;
+
+            // PickLocation(&food, width, height, scale);
+        }
+
+        // growing logic
+        // shift tail segments, excluding the last one
+        for (i = 0; i < total-1; i++) {
+            snake.tail[i] = snake.tail[i+1];
+        }
+
+        // update last tail segment with current head position
+        if (total >= 1) {
+            Vector2 tail_piece = {snake.pos.x, snake.pos.y};
+            snake.tail[total-1] = tail_piece;
+        }
+
+        // for (i = 0; i < total; i ++) {
+        //     DrawRectangle(snake.tail[i].x+1, snake.tail[i].y, scale, scale, RAYWHITE);
+        // }
+
+        // if snake dies, free(snake.tail)
+
+        // update position
         snake.pos.x += snake.xspeed * scale;
         snake.pos.y += snake.yspeed * scale;
 
@@ -49,22 +98,11 @@ int main()
 
         SnakeDir(&snake, speed);
 
-        if (Vector2Distance(snake.pos, food) < 1) {
-            total++;
-            size_t new_size = total * sizeof(Vector2);
-            tail = realloc(tail, new_size);
-            // if (tail == NULL) 
-            //     printf("tail>0 NULL\n\n");
-            // else
-            //     printf("%zu\n", new_size);
-
-            PickLocation(&food, width, height, scale);
-        }
-
-        // update tail pos
-
         BeginDrawing();
         ClearBackground(BLACK);
+
+        for (i = 0; i < total; i ++) 
+            DrawRectangle(snake.tail[i].x, snake.tail[i].y, scale, scale, RAYWHITE);
 
         DrawRectangle(snake.pos.x, snake.pos.y, scale, scale, RAYWHITE);
         DrawRectangle(food.x, food.y, scale, scale, GREEN);
@@ -72,6 +110,7 @@ int main()
         EndDrawing();
     }
 
+    free(snake.tail);
     CloseWindow();
 
     return 0;
